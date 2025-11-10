d4a96186b266977e09b756bfe805dc03698a770b
JuliAlchemDev <julialchem@gmail.com>
2025-11-10 12:48:49 +0100

    feat: render testimonials with semantic HTML, BEM and images

--- React-starter-kit/index.jsx
+++ React-starter-kit/index.jsx
@@ -1,5 +1,53 @@
+const { useState, useEffect } = React;
+
 export const App = () => {
-  return(
-    <h1>Hello World!</h1>
+  const [testimonials, setTestimonials] = useState([]);
+
+  const fetchTestimonials = async () => {
+    try {
+      const res = await fetch("./testimonials-data.json", {
+        cache: "no-store",
+      });
+      if (!res.ok) {
+        throw new Error("Response was not ok");
+      }
+      const data = await res.json();
+      setTestimonials(data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchTestimonials();
+  }, []);
+
+  return (
+    <main className="testimonials-grid">
+      {testimonials.map(({ name, role, summary, story }, index) => {
+        const imageName = name.split(" ")[0].toLowerCase();
+        const imageSource = `./assets/images/image-${imageName}.jpg`;
+
+        return (
+          <section key={index} className="testimonials-grid__card">
+            <header className="testimonials-grid__card-header">
+              <img
+              className="testimonials-grid__card-header__image"
+              src={imageSource}
+              alt={name}
+              width='56'
+              height='56'
+            />
+
+            <h2 className="testimonials-grid__card-header__name">{name}</h2>
+            <p className="testimonials-grid__card-header__role">{role}</p>
+            </header>
+            
+            <h3 className="testimonials-grid__card-summary">{summary}</h3>
+            <q className="testimonials-grid__card-story"> {story} </q>
+          </section>
+        );
+      })}
+    </main>
   );
 };
