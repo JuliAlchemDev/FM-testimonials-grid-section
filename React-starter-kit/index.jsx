8ca7aa79a19084986be734853492508989efdad8
JuliAlchemDev <julialchem@gmail.com>
2025-11-16 09:11:04 +0100

    refactor: add utility classes and integrate card-styles.json

--- React-starter-kit/index.jsx
+++ React-starter-kit/index.jsx
@@ -1,50 +1,73 @@
 const { useState, useEffect } = React;
 
+
 export const App = () => {
   const [testimonials, setTestimonials] = useState([]);
+  const [cardStyles, setCardStyles] = useState({});
+
 
   const fetchTestimonials = async () => {
     try {
-      const res = await fetch("./testimonials-data.json", {
+      const testimonialsRes = await fetch("./testimonials-data.json", {
         cache: "no-store",
       });
-      if (!res.ok) {
+      if (!testimonialsRes.ok) {
         throw new Error("Response was not ok");
       }
-      const data = await res.json();
-      setTestimonials(data);
-    } catch (err) {
-      console.error(err.message);
+      const testimonialsData = await testimonialsRes.json();
+      setTestimonials(testimonialsData);
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
+  const fetchCardStyles = async () => {
+    try {
+      const stylesRes = await fetch("./card-styles.json", {
+        cache: "no-store",
+      });
+      if(!stylesRes.ok){
+        throw new Error("There were some problems to get styles...")
+      }
+      const stylesData = await stylesRes.json();
+      setCardStyles(stylesData);
+    } catch (error){
+      console.error(error.message);
+    }
+  }
+
   useEffect(() => {
     fetchTestimonials();
+    fetchCardStyles();
   }, []);
-
+ 
   return (
     <main className="testimonials-grid">
-      {testimonials.map(({ name, role, summary, story }, index) => {
+      {testimonials.map(({ name, role, summary, story, style }, index) => {
+        const cardStyle = cardStyles[style] || {};
+       
         const imageName = name.split(" ")[0].toLowerCase();
         const imageSource = `./assets/images/image-${imageName}.jpg`;
 
+    
+
         return (
-          <section key={index} className="testimonials-grid__card">
+          <section key={index} className={`testimonials-grid__card ${cardStyle.card || ''}`}>
             <header className="testimonials-grid__card-header">
               <img
-              className="testimonials-grid__card-header__avatar"
+              className={`testimonials-grid__card-header__avatar ${cardStyle.avatar || ''}`}
               src={imageSource}
               alt={name}
               width='28'
               height='28'
             />
 
-            <h2 className="testimonials-grid__card-header__name">{name}</h2>
-            <p className="testimonials-grid__card-header__role">{role}</p>
+            <h2 className={`testimonials-grid__card-header__name ${cardStyle.name || ''}`}>{name}</h2>
+            <p className={`testimonials-grid__card-header__role ${cardStyle.role || ''}`}>{role}</p>
             </header>
             
-            <h3 className="testimonials-grid__card-summary">{summary}</h3>
-            <q className="testimonials-grid__card-story"> {story} </q>
+            <h3 className={`testimonials-grid__card-summary ${cardStyle.summary || ''}`}>{summary}</h3>
+            <q className={`testimonials-grid__card-story ${cardStyle.story || ''}`}> {story} </q>
           </section>
         );
       })}
